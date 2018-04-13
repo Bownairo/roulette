5fb1384e60821e3c2ef6c15308d91ad7dbc2f9b9
bownairo <eerokelly@gmail.com>
2018-04-12 21:38:24 -0400

    Create super basic language pages

--- roulette/core/views.py
+++ roulette/core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
+from .models import Language
+
+class LanguageDetailView(generic.DetailView):
+    model = Language
+
+class LanguageListView(generic.ListView):
+    model = Language
