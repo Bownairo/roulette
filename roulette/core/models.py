a4c5cffd0e3bffc4b407f759ccc312f6537da9fe
bownairo <eerokelly@gmail.com>
2018-04-12 21:57:15 -0400

    Fix incorrect ImageField syntax

--- roulette/core/models.py
+++ roulette/core/models.py
@@ -5,4 +5,4 @@ class Language(models.Model):
     name = models.CharField(max_length=30)
     url = models.URLField()
     description = models.TextField()
-    thumbnail = models.ImageField(height_field=500,width_field=500)
+    thumbnail = models.ImageField()
