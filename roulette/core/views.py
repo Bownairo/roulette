b2cf6d978a89a87572f12130e778d181efa53533
bownairo <eerokelly@gmail.com>
2018-04-15 19:38:35 -0400

    Rearrange views to be nicer

--- roulette/core/views.py
+++ roulette/core/views.py
@@ -4,14 +4,14 @@ from django.views import generic
 # Create your views here.
 from .models import Language, Idea
 
-class LanguageDetailView(generic.DetailView):
-    model = Language
-
 class LanguageListView(generic.ListView):
     model = Language
 
-class IdeaDetailView(generic.DetailView):
-    model = Idea
+class LanguageDetailView(generic.DetailView):
+    model = Language
 
 class IdeaListView(generic.ListView):
+    model = Idea
+
+class IdeaDetailView(generic.DetailView):
     model = Idea
