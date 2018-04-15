3d3f2b64082586c25578e0847ab4e257c7b35475
bownairo <eerokelly@gmail.com>
2018-04-15 19:37:51 -0400

    Create Project model

--- roulette/core/models.py
+++ roulette/core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Language(models.Model):
@@ -12,3 +13,11 @@ class Idea(models.Model):
     description = models.TextField()
     thumbnail = models.ImageField()
     url = models.URLField()
+
+class Project(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    github_url = models.URLField()
+    language = models.ForeignKey(Language, null=True, on_delete=models.SET_NULL)
+    idea = models.ForeignKey(Idea, null=True, on_delete=models.SET_NULL)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
