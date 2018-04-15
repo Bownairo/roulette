206732dfc1ea02ab59e8c598794062ca27939f1e
bownairo <eerokelly@gmail.com>
2018-04-15 19:02:13 -0400

    Rearrange models to look nicer

--- roulette/core/models.py
+++ roulette/core/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 class Language(models.Model):
     name = models.CharField(max_length=30)
-    url = models.URLField()
     description = models.TextField()
     thumbnail = models.ImageField()
+    url = models.URLField()
 
 class Idea(models.Model):
     name = models.CharField(max_length=30)
