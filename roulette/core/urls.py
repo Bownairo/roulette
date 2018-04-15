9478715845125a728102989f14a452d675b5bf8d
bownairo <eerokelly@gmail.com>
2018-04-15 19:14:31 -0400

    Create the views and map the urls

--- roulette/core/urls.py
+++ roulette/core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import LanguageListView, LanguageDetailView
+from .views import LanguageListView, LanguageDetailView, IdeaListView, IdeaDetailView
 
 urlpatterns = [
-    path('', LanguageListView.as_view()),
+    path('languages/', LanguageListView.as_view()),
     path('<int:pk>/', LanguageDetailView.as_view()),
+    path('ideas/', IdeaListView.as_view()),
+    path('<int:pk>/', IdeaDetailView.as_view()),
 ]
--- roulette/core/views.py
+++ roulette/core/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 from django.views import generic
 
 # Create your views here.
-from .models import Language
+from .models import Language, Idea
 
 class LanguageDetailView(generic.DetailView):
     model = Language
 
 class LanguageListView(generic.ListView):
     model = Language
+
+class IdeaDetailView(generic.DetailView):
+    model = Idea
+
+class IdeaListView(generic.ListView):
+    model = Idea
