7fef1fbbe9671d7fec47238659ea245f3fd34ea6
bownairo <eerokelly@gmail.com>
2018-04-15 20:05:52 -0400

    Create a base page

--- roulette/core/urls.py
+++ roulette/core/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
+from django.views.generic import TemplateView
 from .views import LanguageListView, LanguageDetailView, IdeaListView, IdeaDetailView, ProjectListView, ProjectDetailView
 
+app_name = 'core'
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='core/index.html')),
     path('languages/', LanguageListView.as_view()),
     path('languages/<int:pk>/', LanguageDetailView.as_view()),
     path('ideas/', IdeaListView.as_view()),
