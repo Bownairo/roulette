a36aead0590aafc594d8ac7b956224665a7efb8f
bownairo <eerokelly@gmail.com>
2018-04-15 23:53:48 -0400

    Create views for updating and deleting

--- roulette/core/urls.py
+++ roulette/core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from django.views.generic import TemplateView
-from .views import LanguageListView, LanguageDetailView, LanguageCreateView, IdeaListView, IdeaDetailView, IdeaCreateView, ProjectListView, ProjectDetailView, ProjectCreateView
+from .views import *
 
 app_name = 'core'
 urlpatterns = [
@@ -9,10 +9,16 @@ urlpatterns = [
     path('languages/', LanguageListView.as_view(), name='languages'),
     path('languages/<int:pk>/', LanguageDetailView.as_view(), name='language'),
     path('languages/create/', LanguageCreateView.as_view(template_name='core/form.html'), name='language-add'),
+    path('languages/update/<int:pk>/', LanguageUpdateView.as_view(template_name='core/form.html'), name='language-update'),
+    path('languages/delete/<int:pk>/', LanguageDeleteView.as_view(template_name='core/form.html'), name='language-delete'),
     path('ideas/', IdeaListView.as_view(), name='ideas'),
     path('ideas/<int:pk>/', IdeaDetailView.as_view(), name='idea'),
     path('ideas/create/', IdeaCreateView.as_view(template_name='core/form.html'), name='idea-add'),
+    path('ideas/update/<int:pk>/', LanguageUpdateView.as_view(template_name='core/form.html'), name='idea-update'),
+    path('ideas/delete/<int:pk>/', LanguageDeleteView.as_view(template_name='core/form.html'), name='idea-delete'),
     path('projects/', ProjectListView.as_view(), name='projects'),
     path('projects/<int:pk>/', ProjectDetailView.as_view(), name='project'),
     path('projects/create/', ProjectCreateView.as_view(template_name='core/form.html'), name='project-add'),
+    path('projects/update/<int:pk>/', LanguageUpdateView.as_view(template_name='core/form.html'), name='project-update'),
+    path('projects/delete/<int:pk>/', LanguageDeleteView.as_view(template_name='core/form.html'), name='project-delete'),
 ]
--- roulette/core/views.py
+++ roulette/core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
+from django.urls import reverse_lazy
 
 # Create your views here.
 from .models import Language, Idea, Project
@@ -14,6 +15,14 @@ class LanguageCreateView(generic.edit.CreateView):
     model = Language
     fields = '__all__'
 
+class LanguageUpdateView(generic.edit.UpdateView):
+    model = Language
+    fields = '__all__'
+
+class LanguageDeleteView(generic.edit.DeleteView):
+    model = Language
+    success_url = reverse_lazy('language-list')
+
 class IdeaListView(generic.ListView):
     model = Idea
 
@@ -24,6 +33,14 @@ class IdeaCreateView(generic.edit.CreateView):
     model = Idea
     fields = '__all__'
 
+class IdeaUpdateView(generic.edit.UpdateView):
+    model = Idea
+    fields = '__all__'
+
+class IdeaDeleteView(generic.edit.DeleteView):
+    model = Idea
+    success_url = reverse_lazy('idea-list')
+
 class ProjectListView(generic.ListView):
     model = Project
 
@@ -33,3 +50,11 @@ class ProjectDetailView(generic.DetailView):
 class ProjectCreateView(generic.edit.CreateView):
     model = Project
     fields = '__all__'
+
+class ProjectUpdateView(generic.edit.UpdateView):
+    model = Project
+    fields = '__all__'
+
+class ProjectDeleteView(generic.edit.DeleteView):
+    model = Project
+    success_url = reverse_lazy('project-list')
