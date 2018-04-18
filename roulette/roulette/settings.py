e7a2d34185703de22692cf08cdec35d615c19e6f
bownairo <eerokelly@gmail.com>
2018-04-18 18:38:29 -0400

    Switch ALLOWED_HOSTS back, again

--- roulette/roulette/settings.py
+++ roulette/roulette/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.69']
+#ALLOWED_HOSTS = ['192.168.1.69']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
